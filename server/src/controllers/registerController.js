2b19af2a1d84a8e112db2774d3ae04803bfc8465
Yusef Codes <youcefcopy@gmail.com>
2026-05-10 12:04:53 +0100

    fixing the error handling

--- server/src/controllers/registerController.js
+++ server/src/controllers/registerController.js
@@ -3,7 +3,7 @@ import createError from '../utils/createError.js';
 const handleRegister = (req, res, next) => {
     const {name, email} = req.body;
 
-    // if (!name || !email) return next( createError(400, 'Please, include your name and email!'));
+    if (!name || !email) return next( createError(400, 'Please, include your name and email!'));
 
     res.status(200).send('It is working')
 }
--- server/src/middlewares/handleError.js
+++ server/src/middlewares/handleError.js
@@ -1,7 +1,10 @@
 const handleError = (err, req, res, next) => {
+        console.log('ERROR RECEIVED:', err);
+    console.log('STATUS:', err.status);
+    console.log('MESSAGE:', err.message);
+
     const statusCode = err.status || 500;
     const message = statusCode === 500 ? 'Internal Server Error!' : err.message;
-    console.log(message);
     res.status(statusCode).json(message);
 }
 
