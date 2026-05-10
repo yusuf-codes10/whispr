a0215d08e56077fec7eb2cbcbf5e6ba8b8136c4c
Yusef Codes <youcefcopy@gmail.com>
2026-05-10 18:23:08 +0100

    awaiting for channels and fixing channel config typo

--- server/src/controllers/chatController.js
+++ server/src/controllers/chatController.js
@@ -1,7 +1,7 @@
 import createError from "../utils/createError.js";
 import { StreamChat } from "stream-chat";
-import Groq from 'groq-sdk';
-import pool from '../db/pool.js';
+import Groq from "groq-sdk";
+import pool from "../db/pool.js";
 
 // Initialize Stream Client
 const chatClient = StreamChat.getInstance(
@@ -27,37 +27,43 @@ const handleChat = async (req, res, next) => {
     if (!userResponse.users.length)
       return next(createError(404, "user not found. Please register first"));
 
-        // checking if user exists in db
-    const existingUser = await pool.query('SELECT * FROM users WHERE users.user_id = $1', [userId]);
+    // checking if user exists in db
+    const existingUser = await pool.query(
+      "SELECT * FROM users WHERE users.user_id = $1",
+      [userId],
+    );
 
     if (!existingUser.rows.length) {
-      return next(createError(404, 'User not found! please register first!'));
+      return next(createError(404, "User not found! please register first!"));
     }
 
     // send a message to the groq
     const response = await groq.chat.completions.create({
       model: "llama-3.3-70b-versatile", // free and very capable
-      messages: [{role: 'user', content: message}],
+      messages: [{ role: "user", content: message }],
     });
     console.log(response.choices[0].message.content);
 
     const aiMessage = response.choices[0].message.content;
 
     // Save chat to db
-    await pool.query('INSERT INTO chats (user_id, message, reply) VALUES ($1, $2, $3)',[userId, message, aiMessage]);
+    await pool.query(
+      "INSERT INTO chats (user_id, message, reply) VALUES ($1, $2, $3)",
+      [userId, message, aiMessage],
+    );
 
     // creating a channel for the 1on1 converstaion
-    const channel = chatClient.channel('messaging', `chat-${userId}`, {
-      name: 'whispr',
-      create_by_id: 'whisper_bot'
-    })
+    const channel = chatClient.channel("messaging", `chat-${userId}`, {
+      name: "whispr",
+      created_by_id: "whisper_bot", // fixed
+    });
 
-    channel.create();
-    channel.sendMessage({text: aiMessage, user_id: 'whisper_bot'});
+    await channel.create();
+    await channel.sendMessage({ text: aiMessage, user_id: "whisper_bot" });
 
-    res.status(200).json({reply: aiMessage});
+    res.status(200).json({ reply: aiMessage });
   } catch (error) {
-    console.log('error getting the response', error);
+    console.log("error getting the response", error);
     next(error);
   }
 };
