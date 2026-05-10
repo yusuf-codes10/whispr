942fa6783a68b60cf1629e3760ab27256e699b6b
Yusef Codes <youcefcopy@gmail.com>
2026-05-10 13:02:56 +0100

    testing StreamChat and the response

--- server/src/controllers/registerController.js
+++ server/src/controllers/registerController.js
@@ -1,11 +1,29 @@
-import createError from '../utils/createError.js';
+import createError from "../utils/createError.js";
+import { StreamChat } from 'stream-chat';
 
-const handleRegister = (req, res, next) => {
-    const {name, email} = req.body;
+// Initialize Stream Client
+const chatClient = StreamChat.getInstance(
+  process.env.STREAM_API_KEY,
+  process.env.STREAM_API_SECRET
+);
 
-    if (!name || !email) return next( createError(400, 'Please, include your name and email!'));
+const handleRegister = async (req, res, next) => {
+  const { name, email } = req.body;
 
-    res.status(200).send('It is working')
-}
+  if (!name || !email)
+    return next(createError(400, "Please, include your name and email!"));
+
+  try {
+    const userId = email.replace(/[^a-zA-Z0-9_-]/g, "_");
+
+    // Check if user exists
+    const userResponse = await chatClient.queryUsers({ id: { $eq: userId } });
+
+    console.log(userResponse);
+    res.status(200).send("It is working");
+  } catch (error) {
+    next(error);
+  }
+};
 
 export default handleRegister;
