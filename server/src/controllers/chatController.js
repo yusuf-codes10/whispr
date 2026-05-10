6efa3ac6c841b095f98ebbea3b358ecef9f0bf4b
Yusef Codes <youcefcopy@gmail.com>
2026-05-10 14:45:25 +0100

    testing the chat route | we still need to solve the race condition later

--- server/src/controllers/chatController.js
+++ server/src/controllers/chatController.js
@@ -1,5 +1,32 @@
-const handleChat = async (req, res, next) => {
+import createError from "../utils/createError.js";
+import { StreamChat } from 'stream-chat';
 
-}
+// Initialize Stream Client
+const chatClient = StreamChat.getInstance(
+  process.env.STREAM_API_KEY,
+  process.env.STREAM_API_SECRET
+);
+
+const handleChat = async (req, res, next) => {
+      console.log("KEY:", process.env.STREAM_API_KEY);
+console.log("SECRET:", process.env.STREAM_API_SECRET);
+  const { message, userId } = req.body;
+
+  if (!message || !userId)
+    return next(createError(400, "The message and user id are requierd!"));
+
+  try {
+    // Verify user exists
+    const userResponse = await chatClient.queryUsers({ id: userId });
+
+    if (!userResponse.users.length)
+      return next(createError(404, 'user not found. Please register first'));
+
+    res.status(200).send('It is working');
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
 
 export default handleChat;
--- server/src/routes/chat.route.js
+++ server/src/routes/chat.route.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import handleChat from '../controllers/chatController.js';
 
 const router = express.Router();
 
