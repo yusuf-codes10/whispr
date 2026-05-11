f24c4151e4923c420d98424e1ab2b85e2b91a872
Yusef Codes <youcefcopy@gmail.com>
2026-05-11 14:20:51 +0100

    chat view scrool to bottom & fetching chats

--- client/src/views/ChatView.vue
+++ client/src/views/ChatView.vue
@@ -1,8 +1,33 @@
 <script setup>
+import { onMounted, nextTick } from 'vue'
+import { useUserStore } from '@/stores/userStore'
+import { useChatStore } from '@/stores/chatStore'
+import { useRouter } from 'vue-router'
+
+const userStore = useUserStore()
+const chatStore = useChatStore()
+const router = useRouter()
+
+// check if user is logged in
+if (!userStore.userId) {
+  router.push('/')
+}
+
+// auto scrool to bottom
+const scrollToBottom = () => {
+  nextTick(() => {
+    const chatContainer = document.getElementById('chat-container')
+    if (chatContainer) chatContainer.scrollTop = chatContainer.scrollHeight
+  })
+}
+
+onMounted(() => {
+  chatStore.fetchChatMessages().then(() => scrollToBottom())
+})
 </script>
 
 <template>
-  <div>Chat</div>
+  <div class="h-screen bg-background flex flex-col"></div>
 </template>
 
 <style scoped>
