9c1b850dda9ef72613ab09b132b9d1d75287dfe9
Yusef Codes <youcefcopy@gmail.com>
2026-05-11 12:25:23 +0100

    adding a logo

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,10 +1,15 @@
 <script setup>
+import whisprLogo from '@/assets/whispr.png'
 </script>
 
 <template>
-  <div class="h-screen bg-background">
-    <div class="bg-card w-24 h-24">
-      <h1 class="title">Home Page</h1>
+  <div class="h-screen bg-background flex items-center justify-center">
+    <div class="bg-card py-2 px-4">
+      <img :src="whisprLogo" alt="" class="w-25 h-25 mx-auto" />
+      <h1 class="title text-center">Welcome to Whispr</h1>
+      <input type="text" class="w-full mb-2 bg-popover rounded" placeholder="Name" />
+      <input type="text" class="w-full mb-2 bg-popover rounded" placeholder="Email" />
+      <button class="button w-full">Go to chat</button>
     </div>
   </div>
 </template>
