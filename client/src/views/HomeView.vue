64e2644db4721f022e7d09c45d7c37c0c1f6996f
Yusef Codes <youcefcopy@gmail.com>
2026-05-11 12:17:39 +0100

    making the dark color the defualt

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -2,7 +2,11 @@
 </script>
 
 <template>
-  <div class="h-screen bg-gray-800">Home Page</div>
+  <div class="h-screen bg-background">
+    <div class="bg-card w-24 h-24">
+      <h1 class="title">Home Page</h1>
+    </div>
+  </div>
 </template>
 
 <style scoped>
