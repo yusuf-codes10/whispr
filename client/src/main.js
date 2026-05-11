efa52468d67d572fd6247a8a1778743e52f06f61
Yusef Codes <youcefcopy@gmail.com>
2026-05-11 12:47:52 +0100

    adding presist as 3rd arg for composition api

--- client/src/main.js
+++ client/src/main.js
@@ -5,10 +5,14 @@ import App from './App.vue'
 import router from './router'
 
 import '@/assets/main.css'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 const app = createApp(App)
 
-app.use(createPinia())
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
--- client/src/stores/userStore.js
+++ client/src/stores/userStore.js
@@ -1,21 +1,25 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useUserStore = defineStore('user', () => {
-  // * state
-  const userId = ref('')
-  const username = ref('')
+export const useUserStore = defineStore(
+  'user',
+  () => {
+    // * state
+    const userId = ref('')
+    const username = ref('')
 
-  // * actions
-  const setUser = (id, name) => {
-    userId.value = id
-    username.value = name
-  }
+    // * actions
+    const setUser = (id, name) => {
+      userId.value = id
+      username.value = name
+    }
 
-  const logUserOut = () => {
-    userId.value = ''
-    username.value = ''
-  }
+    const logUserOut = () => {
+      userId.value = ''
+      username.value = ''
+    }
 
-  return { userId, username, setUser, logUserOut }
-})
+    return { userId, username, setUser, logUserOut }
+  },
+  { persist: true },
+)
