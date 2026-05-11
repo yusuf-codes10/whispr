b5e099e480f2bf21735d8cefc64ca238971a6164
Yusef Codes <youcefcopy@gmail.com>
2026-05-11 14:22:51 +0100

    format message function

--- client/src/views/ChatView.vue
+++ client/src/views/ChatView.vue
@@ -21,6 +21,22 @@ const scrollToBottom = () => {
   })
 }
 
+// Format message
+const formatMessage = (text) => {
+  if (!text) return ''
+
+  return text
+    .replace(/\n/g, '<br>') // Preserve line breaks
+    .replace(/\*\*(.*?)\*\*/g, '<b>$1</b>') // Bold text
+    .replace(/\*(.*?)\*/g, '<i>$1</i>') // Italic text
+    .replace(/`(.*?)`/g, '<code>$1</code>') // Inline code
+    .replace(/(?:^|\n)- (.*?)(?:\n|$)/g, '<li>$1</li>') // Bullet points
+    .replace(/(?:^|\n)(\d+)\. (.*?)(?:\n|$)/g, '<li>$1. $2</li>') // Numbered lists
+    .replace(/<\/li>\n<li>/g, '</li><li>') // Ensure list continuity
+    .replace(/<li>/, '<ul><li>') // Wrap in `<ul>`
+    .replace(/<\/li>$/, '</li></ul>') // Close the `<ul>`
+}
+
 onMounted(() => {
   chatStore.fetchChatMessages().then(() => scrollToBottom())
 })
