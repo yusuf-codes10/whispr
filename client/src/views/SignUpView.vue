d21b542e57433eccd18fe33e71dc27cbdef37d93
Yusef Codes <youcefcopy@gmail.com>
2026-05-25 19:57:13 +0100

    style:  fixing margins

--- client/src/views/SignUpView.vue
+++ client/src/views/SignUpView.vue
@@ -1,19 +1,24 @@
 <script setup>
-import whisprLogo from '@/assets/whispr.png'
+// import whisprLogo from '@/assets/whispr.png'
 </script>
 
 <template>
   <div class="h-screen bg-background flex items-center justify-center">
     <div class="bg-card py-8 px-8 rounded-xl border border-border shadow-lg w-80">
-      <img :src="whisprLogo" alt="" class="w-25 h-25 mx-auto" />
+      <!-- <img :src="whisprLogo" alt="" class="w-25 h-25 mx-auto" /> -->
       <h1 class="title text-center mb-6">Welcome to Whispr</h1>
       <p class="text mb-2">You’ll get smarter responses and can upload files, images, and more.</p>
       <button class="w-full ghost-button" @click="goToChat">
         {{ loading ? 'Logging in...' : 'Continue with Google' }}
       </button>
+
+      <hr />
+      <p class="text-center">OR</p>
+
+      <hr />
       <input
         type="email"
-        class="w-full mb-4 bg-input text-foreground placeholder-muted-foreground border border-border focus:border-primary focus:ring-2 focus:ring-primary/20 outline-none rounded px-3 py-2 text-sm transition-all rounded-radius"
+        class="w-full my-4 bg-input text-foreground placeholder-muted-foreground border border-border focus:border-primary focus:ring-2 focus:ring-primary/20 outline-none rounded px-3 py-2 text-sm transition-all radius"
         placeholder="Email Adress"
         v-model="email"
       />
