b4261e54495d0b8856176da070c95a8676101fb9
Yusef Codes <youcefcopy@gmail.com>
2026-05-25 19:08:28 +0100

    saparate login & register views

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,5 +1,7 @@
 import ChatView from '@/views/ChatView.vue'
 import HomeView from '@/views/HomeView.vue'
+import SignInView from '@/views/SignInView.vue'
+import SignUpView from '@/views/SignUpView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
@@ -7,6 +9,18 @@ const routes = [
     path: '/',
     name: 'Home',
     component: HomeView,
+  },
+  // login
+  {
+    path: '/sign-in',
+    name: 'SignIn',
+    component: SignInView,
+  },
+  // register
+  {
+    path: '/sign-up',
+    name: 'SignUp',
+    component: SignUpView,
   },
   {
     path: '/chat',
