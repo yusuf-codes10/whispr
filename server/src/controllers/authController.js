d40154acb5c2b9e2318d01cc176aba55251a976f
Yusef Codes <youcefcopy@gmail.com>
2026-05-26 21:35:36 +0100

    handle callback function

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -17,3 +17,43 @@ export const getGoogleAuthUrl = (req, res) => {
   });
   res.json({ url });
 };
+
+// Step 2: Handle the code Vue sends back
+// Vue picks up ?code=xyz and sends it here
+export const handleGoogleCallback = async (req, res) => {
+  const { code } = req.body;
+
+  // Exchange code for tokens
+  const { tokens } = await client.getToken(code);
+  client.setCredentials(tokens);
+
+  // Get user info from Google
+  const ticket = await client.verifyIdToken({
+    idToken: tokens.id_token,
+    audience: process.env.GOOGLE_CLIENT_ID,
+  });
+
+  const { sub: google_id, email, name, picture } = ticket.getPayload();
+
+  // Check if user exists in YOUR database
+  const user = await pool.query("SELECT * FROM users WHERE google_id = $1", [
+    google_id,
+  ]);
+
+  if (user.rows.length === 0) {
+    // New user — insert them
+    user = await pool.query(
+      "INSERT INTO users (google_id, email, name, avatar_url) VALUES ($1, $2, $3, $4) RETURNING *",
+      [google_id, email, name, picture],
+    );
+  }
+
+    // Sign your own JWT
+  const token = jwt.sign(
+    { id: user.rows[0].id },
+    process.env.JWT_SECRET,
+    { expiresIn: '7d' }
+  )
+
+  res.json({ token })
+};
