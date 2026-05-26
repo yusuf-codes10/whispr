a5a1b674f03b043363ab1c56660303c88832733d
Yusef Codes <youcefcopy@gmail.com>
2026-05-26 21:04:03 +0100

    setting up the client

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,3 +1,9 @@
 import { OAuth2Client } from "google-auth-library";
 import jwt from 'jsonwebtoken';
 import pool from '../db/pool.js';
+
+const client = new OAuth2Client(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    process.env.GOOGLE_REDIRECT_URI // http://localhost:5173/auth/callbacku
+);
