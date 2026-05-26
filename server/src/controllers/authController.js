9d28d528d2b8ddcd6911aa786a03d6beec49cbdc
Yusef Codes <youcefcopy@gmail.com>
2026-05-26 21:09:32 +0100

    getGoogleAuthUrl function

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,9 +1,19 @@
 import { OAuth2Client } from "google-auth-library";
-import jwt from 'jsonwebtoken';
-import pool from '../db/pool.js';
+import jwt from "jsonwebtoken";
+import pool from "../db/pool.js";
 
 const client = new OAuth2Client(
-    process.env.GOOGLE_CLIENT_ID,
-    process.env.GOOGLE_CLIENT_SECRET,
-    process.env.GOOGLE_REDIRECT_URI // http://localhost:5173/auth/callbacku
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  process.env.GOOGLE_REDIRECT_URI, // http://localhost:5173/auth/callbacku
 );
+
+// Step 1: Generate Google login URL
+// Vue calls this to get the URL to redirect the user to
+export const getGoogleAuthUrl = (req, res) => {
+  const url = client.generateAuthUrl({
+    access_type: "offline",
+    scope: ["openid", "email", "profile"],
+  });
+  res.json({ url });
+};
