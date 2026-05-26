238ca0f4942399b03a5f8e7c41a68f091966a303
Yusef Codes <youcefcopy@gmail.com>
2026-05-26 21:36:55 +0100

    google auth routes

--- server/src/routes/signup.route.js
+++ server/src/routes/signup.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import {handleGoogleCallback, getGoogleAuthUrl} from '../controllers/authController.js';
 
 const router = express.Router();
 
 // redirects user to google login page
-router.get('/google');
+router.get('/google', getGoogleAuthUrl);
 
 // recieves the code from google
-router.get('/google/callback');
+router.get('/google/callback', handleGoogleCallback);
 
 export default router;
