038d2226f6c39c301939beeaabefbcd7dd93dc74
Yusef Codes <youcefcopy@gmail.com>
2026-05-27 12:14:48 +0100

    client: sending the cookie with axios

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true, // ← this sends cookies automatically
 })
 
 export default api
