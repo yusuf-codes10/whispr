9c8a82c0b0fba9732f23204fd7168d308486b3c3
Yusef Codes <youcefcopy@gmail.com>
2026-05-27 20:15:09 +0100

    we better use the google_id for stream chat

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -45,7 +45,22 @@ console.log("SECRET:", process.env.STREAM_API_SECRET);
       audience: process.env.GOOGLE_CLIENT_ID,
     });
 
+        // Check if user exists in stream
+
+        if (!userResponse.users.length) {
+      // Add new user to stream
+      await chatClient.upsertUser({
+        id: userId,
+        name: name,
+        email: email,
+        role: "user",
+      });
+    }
+    // 1. Get user info from Google
     const { sub: google_id, email, name, picture } = ticket.getPayload();
+
+    // 2. Check/create in Stream using google_id
+    const userResponse = await chatClient.queryUsers({ id: { $eq: google_id } });
 
     // Check if user exists in YOUR database
     let user = await pool.query("SELECT * FROM users WHERE google_id = $1", [
