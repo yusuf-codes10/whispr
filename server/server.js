83b701a6e6489176017a6a7ff7e7164bc684326c
Yusef Codes <youcefcopy@gmail.com>
2026-05-27 12:13:14 +0100

    setting up the cookie parser

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 import handleError from './src/middlewares/handleError.js';
 import catchAll from './src/middlewares/catchAll.js';
@@ -22,6 +23,7 @@ const app = express();
 
 app.use(logger);
 app.use(cors());
+app.use(cookieParser);
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
