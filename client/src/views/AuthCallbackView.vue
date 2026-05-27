887e4b0968a50295e06f3cb527e4be2116592bdf
Yusef Codes <youcefcopy@gmail.com>
2026-05-27 10:47:46 +0100

    we need to grab the code from the url

--- client/src/views/AuthCallbackView.vue
+++ client/src/views/AuthCallbackView.vue
@@ -1,4 +1,16 @@
 <script setup>
+import { onMounted } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+import api from '@/services/api.js'
+
+const router = useRouter()
+const route = useRoute()
+
+onMounted(async () => {
+  const code = route.query.code
+
+  if (!code) return router.push('/sign-up')
+})
 </script>
 
 <template>
