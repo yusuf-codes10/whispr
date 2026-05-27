03ba823a9b845862611a4692421d0a4c7676198c
Yusef Codes <youcefcopy@gmail.com>
2026-05-27 20:55:22 +0100

    new oauth store

--- client/src/stores/authStore.js
+++ client/src/stores/authStore.js
@@ -1,14 +1,26 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import api from '@/services/api.js'
+import { useRouter } from 'vue-router'
 
 export const useAuthStore = defineStore('auth', () => {
   // *state
   const user = ref(null)
+  const router = useRouter()
 
-  const register = async (credentials) => {}
+  const getGoogleUrl = async () => {
+    const response = await api.get('/auth/google')
+    window.location.href = response.data.url
+  }
+
+  const handleCallback = async (code) => {
+    const response = await api.post('/auth/google/callback', { code })
+    router.push('/chat')
+  }
 
   return {
     user,
+    getGoogleUrl,
+    handleCallback,
   }
 })
