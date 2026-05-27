1bd4290de15322f4d16ca885e49fa58a8d0d18e9
Yusef Codes <youcefcopy@gmail.com>
2026-05-27 21:06:29 +0100

    commenting out the old register code

--- server/src/controllers/registerController.js
+++ server/src/controllers/registerController.js
@@ -1,52 +1,52 @@
-import createError from "../utils/createError.js";
-import { StreamChat } from "stream-chat";
-import pool from '../db/pool.js';
+// import createError from "../utils/createError.js";
+// import { StreamChat } from "stream-chat";
+// import pool from '../db/pool.js';
 
-// Initialize Stream Client
-const chatClient = StreamChat.getInstance(
-  process.env.STREAM_API_KEY,
-  process.env.STREAM_API_SECRET,
-);
+// // Initialize Stream Client
+// const chatClient = StreamChat.getInstance(
+//   process.env.STREAM_API_KEY,
+//   process.env.STREAM_API_SECRET,
+// );
 
-const handleRegister = async (req, res, next) => {
-    console.log("KEY:", process.env.STREAM_API_KEY);
-console.log("SECRET:", process.env.STREAM_API_SECRET);
+// const handleRegister = async (req, res, next) => {
+//     console.log("KEY:", process.env.STREAM_API_KEY);
+// console.log("SECRET:", process.env.STREAM_API_SECRET);
 
-// ?TODO: a race condition to fix later
-  const { name, email } = req.body;
+// // ?TODO: a race condition to fix later
+//   const { name, email } = req.body;
 
-  if (!name || !email)
-    return next(createError(400, "Please, include your name and email!"));
+//   if (!name || !email)
+//     return next(createError(400, "Please, include your name and email!"));
 
-  try {
-    const userId = email.replace(/[^a-zA-Z0-9_-]/g, "_");
+//   try {
+//     const userId = email.replace(/[^a-zA-Z0-9_-]/g, "_");
 
-    // Check if user exists in stream
-    const userResponse = await chatClient.queryUsers({ id: { $eq: userId } });
+//     // Check if user exists in stream
+//     const userResponse = await chatClient.queryUsers({ id: { $eq: userId } });
 
-    console.log(userResponse);
+//     console.log(userResponse);
 
-    if (!userResponse.users.length) {
-      // Add new user to stream
-      await chatClient.upsertUser({
-        id: userId,
-        name: name,
-        email: email,
-        role: "user",
-      });
-    }
-    // checking if user exists in db
-    const existingUser = await pool.query('SELECT * FROM users WHERE users.user_id = $1', [userId]);
+//     if (!userResponse.users.length) {
+//       // Add new user to stream
+//       await chatClient.upsertUser({
+//         id: userId,
+//         name: name,
+//         email: email,
+//         role: "user",
+//       });
+//     }
+//     // checking if user exists in db
+//     const existingUser = await pool.query('SELECT * FROM users WHERE users.user_id = $1', [userId]);
 
-    if (!existingUser.rows.length) {
-      console.log('User does not exist, adding...');
-      await pool.query('INSERT INTO users (user_id, name, email) VALUES ($1, $2, $3)', [userId, name, email]);
-    }
+//     if (!existingUser.rows.length) {
+//       console.log('User does not exist, adding...');
+//       await pool.query('INSERT INTO users (user_id, name, email) VALUES ($1, $2, $3)', [userId, name, email]);
+//     }
 
-    res.status(200).json({userId, name, email});
-  } catch (error) {
-    next(error);
-  }
-};
+//     res.status(200).json({userId, name, email});
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
-export default handleRegister;
+// export default handleRegister;
