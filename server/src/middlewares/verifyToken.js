58e6575031e0c741f42a31e6bf0b2bfa4ef9e3c4
Yusef Codes <youcefcopy@gmail.com>
2026-05-27 12:04:53 +0100

    adding a verify token middleware

---
+++ server/src/middlewares/verifyToken.js
@@ -0,0 +1,17 @@
+// middlewares/verifyToken.js
+import jwt from "jsonwebtoken";
+import createError from "../utils/createError.js";
+
+const verifyToken = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) return next(createError(401, "Not authenticated"));
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch {
+    return next(createError(403, "Invalid or expired token"));
+  }
+};
+
+export default verifyToken;
