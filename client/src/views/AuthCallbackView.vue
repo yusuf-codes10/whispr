84e5f88298466c8d80a39613f1df951bb079bb63
Yusef Codes <youcefcopy@gmail.com>
2026-05-27 11:18:58 +0100

    have to debug not user_id null constraint

--- client/src/views/AuthCallbackView.vue
+++ client/src/views/AuthCallbackView.vue
@@ -10,6 +10,17 @@ onMounted(async () => {
   const code = route.query.code
 
   if (!code) return router.push('/sign-up')
+
+  try {
+    const response = await api.post(`${import.meta.env.VITE_API_URL}/auth/google/callback`, {
+      code,
+    })
+    console.log(response)
+    router.push('/chat')
+  } catch (error) {
+    console.log('failed signing in', error)
+    router.push('/sign-up')
+  }
 })
 </script>
 
