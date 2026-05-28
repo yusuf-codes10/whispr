ed027e21182cdd9e778cc8c1d2e8386622eb850a
Yusef Codes <youcefcopy@gmail.com>
2026-05-28 09:35:18 +0100

    setting the new chats controller

---
+++ server/src/controllers/chatsController.js
@@ -0,0 +1,13 @@
+import createError from "../utils/createError";
+import { StreamChat } from "stream-chat";
+import pool from '../db/pool.js';
+import Groq from "groq-sdk";
+
+// Initialize Stream Client
+const chatClient = StreamChat.getInstance(
+  process.env.STREAM_API_KEY,
+  process.env.STREAM_API_SECRET,
+);
+
+// Groq initializer
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
