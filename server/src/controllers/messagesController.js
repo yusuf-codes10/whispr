cdf177654377a7b5cf249cd6bd678e87e74f0fcd
Yusef Codes <youcefcopy@gmail.com>
2026-05-28 13:51:39 +0100

    NOTE: a better rest design to keep in mind

--- server/src/controllers/messagesController.js
+++ server/src/controllers/messagesController.js
@@ -12,6 +12,7 @@ const chatClient = StreamChat.getInstance(
 // Groq initializer
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
+// TODO: POST /chats/:id/messages would be a better RESTFUL design
 export const sendMessage = async (req, res, next) => {
     // grab the chat content from user
     const {content, chatId} = req.body;
