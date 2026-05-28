cdd407aca18551a0226807d1cb5fe09e375db0b4
Yusef Codes <youcefcopy@gmail.com>
2026-05-28 11:11:27 +0100

    fixed generating a title

--- server/src/controllers/chatsController.js
+++ server/src/controllers/chatsController.js
@@ -1,6 +1,6 @@
 import createError from "../utils/createError.js";
 import { StreamChat } from "stream-chat";
-import pool from '../db/pool.js';
+import pool from "../db/pool.js";
 import Groq from "groq-sdk";
 
 // Initialize Stream Client
@@ -14,41 +14,42 @@ const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 // get all chats by user
 export const getAllChats = async (req, res, next) => {
-    const userId = req.user.id  // ← verifyToken already put it here
+  const userId = req.user.id; // ← verifyToken already put it here
 
-    if (!userId) return next(createError(401, 'User id is required'));
+  if (!userId) return next(createError(401, "User id is required"));
 
-    try {
-        const chatHistory = await pool.query('SELECT * FROM chats WHERE chats.user_id = $1', [userId]);
-        res.status(200).json({messages: chatHistory.rows});
-    } catch (error) {
-        console.log('failed getting user chats', error);
-        next(error);
-    }
-}
+  try {
+    const chatHistory = await pool.query(
+      "SELECT * FROM chats WHERE chats.user_id = $1",
+      [userId],
+    );
+    res.status(200).json({ messages: chatHistory.rows });
+  } catch (error) {
+    console.log("failed getting user chats", error);
+    next(error);
+  }
+};
 
 export const createNewChat = async (req, res, next) => {
-    // grab the user id again, form the verifyToken(auth) mw
-    const userId = req.user.id;
+  // grab the user id again, form the verifyToken(auth) mw
+  const userId = req.user.id;
 
-    if (!userId) return next(createError(400, 'user id is required!'));
+  if (!userId) return next(createError(400, "user id is required!"));
 
-    // grab chat info from the user
+  // grab chat info from the user
 
-    try {
-        await pool.query('INSERT INTO chats ()')
-    } catch (error) {
-        
-    }
-}
+  try {
+    await pool.query("INSERT INTO chats ()");
+  } catch (error) {}
+};
 
 export const generateChatTitle = async (req, res, next) => {
-    const message = req.body.message;
-    const prompt = 'Extract a title out of this: ';
-    // send a message to the groq
-    const response = await groq.chat.completions.create({
-      model: "llama-3.3-70b-versatile", // free and very capable
-      messages: prompt + message,
-    });
-    console.log(response.choices[0].message.content);
-}
+  const message = req.body.message;
+  const prompt = "Extract a title out of this, 3-5 words, significant title: ";
+  // send a message to the groq
+  const response = await groq.chat.completions.create({
+    model: "llama-3.3-70b-versatile", // free and very capable
+    messages: [{ role: "user", content: prompt + message }],
+  });
+  console.log(response.choices[0].message.content);
+};
