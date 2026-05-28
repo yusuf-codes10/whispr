5dadc4281958b618862d3c0039f06bca4b19725e
Yusef Codes <youcefcopy@gmail.com>
2026-05-28 09:52:22 +0100

    getAllChats controller

--- server/src/controllers/chatsController.js
+++ server/src/controllers/chatsController.js
@@ -13,6 +13,16 @@ const chatClient = StreamChat.getInstance(
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 // get all chats by user
-const getAllChats = async (req, res, next) => {
+export const getAllChats = async (req, res, next) => {
     const userId = req.user.id  // ← verifyToken already put it here
+
+    if (!userId) return next(createError(401, 'User id is required'));
+
+    try {
+        const chatHistory = await pool.query('SELECT * FROM chats WHERE chats.user_id = $1', [userId]);
+        res.status(200).json({messages: chatHistory.rows});
+    } catch (error) {
+        console.log('failed getting user chats', error);
+        next(error);
+    }
 }
