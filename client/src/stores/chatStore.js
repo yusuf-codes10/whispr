42a6e26f28c7a97ae5316d6282a53bc29307bdca
Yusef Codes <youcefcopy@gmail.com>
2026-05-30 11:25:37 +0100

    fetchMessages function

--- client/src/stores/chatStore.js
+++ client/src/stores/chatStore.js
@@ -2,12 +2,14 @@ import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import { useAuthStore } from '@/stores/authStore.js'
 import api from '@/services/api.js'
+import { useRoute } from 'vue-router'
 
 export const useChatStore = defineStore('chat', () => {
   // * state
   const messages = ref([])
   const isLoading = ref(false)
   const authStore = useAuthStore()
+  const route = useRoute()
 
   // * actions
   const fetchChats = async () => {
@@ -72,5 +74,15 @@ export const useChatStore = defineStore('chat', () => {
     }
   }
 
-  return { messages, isLoading, fetchChats, sendMessage, createChat }
+  const fetchMessages = async () => {
+    const chatId = route.params.id
+    try {
+      const messages = await api.get(`/chats/${chatId}/messages`)
+      return messages
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return { messages, isLoading, fetchChats, sendMessage, createChat, fetchMessages }
 })
