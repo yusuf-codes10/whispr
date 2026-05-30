a79dabbe6dacd39649e40d2cbe3720d44381865e
Yusef Codes <youcefcopy@gmail.com>
2026-05-30 13:43:17 +0100

    rendering the messages with some ui bugs

--- client/src/views/ChatDetailsView.vue
+++ client/src/views/ChatDetailsView.vue
@@ -80,15 +80,15 @@ watch(
         class="flex-1 overflow-y-auto p-4 space-y-4 flex items-center justify-center flex-col gap-2"
       >
         <div
-          v-for="(msg, index) in chatStore.messages"
-          :key="index"
+          v-for="msg in chatStore.messages.messages"
+          :key="msg.id"
           class="flex items-start"
-          :class="msg.role === 'user' ? 'justify-end' : 'justify-start'"
+          :class="msg.sender === 'user' ? 'justify-end' : 'justify-start'"
         >
           <div
             v-html="formatMessage(msg.content)"
             class="max-w-xs px-4 py-2 rounded-lg md:max-w-md"
-            :class="msg.role === 'user' ? 'bg-primary text-white' : 'bg-card text-white'"
+            :class="msg.sender === 'user' ? 'bg-primary text-white' : 'bg-card text-white'"
           ></div>
         </div>
         <div v-if="chatStore.isLoading" class="flex justify-start">
