766c727037209904fc305272d625d86959cefc2e
Yusef Codes <youcefcopy@gmail.com>
2026-05-31 10:14:05 +0100

    a quick drop down menu component

---
+++ client/src/components/DropDownMenu.vue
@@ -0,0 +1,17 @@
+<script setup>
+const props = defineProps({
+  isOpen: {
+    type: Boolean,
+    default: false,
+  },
+})
+</script>
+
+<template>
+  <div
+    v-if="props.isOpen"
+    class="absolute right-0 top-full mt-1 min-w-40 rounded-lg border border-bg-border bg-bg-surface shadow-lg overflow-hidden z-50"
+  >
+    <slot />
+  </div>
+</template>
