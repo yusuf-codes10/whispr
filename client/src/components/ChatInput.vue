577528c24e6194d686009579c84de67e530190b4
Yusef Codes <youcefcopy@gmail.com>
2026-06-02 12:36:18 +0100

    trying to do the mirror trick on the input

--- client/src/components/ChatInput.vue
+++ client/src/components/ChatInput.vue
@@ -14,13 +14,21 @@ const sendMessage = () => {
 
 <template>
   <div class="py-2 px-4 flex">
-    <input
-      v-model="message"
-      @keyup.enter="sendMessage"
-      placeholder="Send a message"
-      type="text"
-      class="flex-1 p-2 rounded-lg bg-secondary text-white focus:outline-none"
-    />
+    <div class="inline-grid">
+      <!-- invisible mirror -->
+      <span class="invisible col-start-1 row-start-1 px-3 py-2 border whitespace-pre">
+        {{ message || ' ' }}
+      </span>
+
+      <!-- actual input -->
+      <input
+        v-model="message"
+        @keyup.enter="sendMessage"
+        placeholder="Send a message"
+        type="text"
+        class="flex-1 px-7 py-3 rounded-lg bg-secondary text-white focus:outline-none"
+      />
+    </div>
     <MainButton @click="sendMessage" title="Send" />
   </div>
 </template>
