ee961a7fbfbeb9af16a4c9a24af8a110e9e89a2d
Yusef Codes <youcefcopy@gmail.com>
2026-06-02 13:16:28 +0100

    adding isMobile state in the store

--- client/src/stores/sideStore.js
+++ client/src/stores/sideStore.js
@@ -3,13 +3,20 @@ import { ref } from 'vue'
 
 export const useSideStore = defineStore('sidebar', () => {
   const isOpen = ref(false)
+  const isMobile = ref(false)
 
   const toggle = () => {
     isOpen.value = !isOpen.value
   }
 
+  const toggleIsMobile = () => {
+    isMobile.value = !isMobile.value
+  }
+
   return {
     isOpen,
+    isMobile,
     toggle,
+    toggleIsMobile,
   }
 })
