31e93ebe531de62dae67f39120574d76020d1e46
Yusef Codes <youcefcopy@gmail.com>
2026-06-05 19:11:36 +0100

    cleaning code
    cleaning the home page

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -2,46 +2,6 @@
 import GhostButton from '@/components/ui/GhostButton.vue'
 import MainButton from '@/components/ui/MainButton.vue'
 import NavBar from '@/layouts/NavBar.vue'
-
-// import { ref } from 'vue'
-// import api from '@/services/api.js'
-// import { useUserStore } from '@/stores/userStore.js'
-// import { useRouter } from 'vue-router'
-// import whisprLogo from '@/assets/whispr.png'
-
-// const userStore = useUserStore()
-// const router = useRouter()
-
-// const name = ref('test')
-// const email = ref('test@gmail.com')
-// const error = ref('')
-// const loading = ref(false)
-
-// const goToChat = async () => {
-//   if (!name.value || !email.value) {
-//     error.value = 'Username and Email are required!'
-//     return
-//   }
-
-//   error.value = ''
-//   loading.value = true
-
-//   try {
-//     const { data } = await api.post(`${import.meta.env.VITE_API_URL}/register`, {
-//       name: name.value,
-//       email: email.value,
-//     })
-
-//     // setting the user in pinia
-//     userStore.setUser(data.userId, data.name)
-
-//     router.push('/chat')
-//   } catch (err) {
-//     error.value = err
-//   } finally {
-//     loading.value = false
-//   }
-// }
 </script>
 
 <template>
@@ -62,16 +22,5 @@ import NavBar from '@/layouts/NavBar.vue'
         <GhostButton title="See it in action" />
       </div>
     </div>
-    <!-- <div class="bg-card py-8 px-8 rounded-xl border border-border shadow-lg w-80"> -->
-    <!-- <img :src="whisprLogo" alt="" class="w-25 h-25 mx-auto" />
-      <h1 class="title text-center mb-6">Welcome to Whispr</h1>
-
-      <RouterLink :to="{ name: 'SignUp' }">
-        <button class="button w-full">
-          {{ loading ? 'Logging in...' : 'Go to Chat' }}
-        </button>
-      </RouterLink>
-      <p v-if="error" class="text-destructive text-center">{{ error }}</p> -->
-    <!-- </div> -->
   </div>
 </template>
