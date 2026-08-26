c0627f088402a0da201213eebd0de87c6de06057
Yusef Codes <youcefcopy@gmail.com>
2026-08-26 20:36:24 +0100

    Navbar updated: no height for the navbar

--- client/src/layouts/NavBar.vue
+++ client/src/layouts/NavBar.vue
@@ -4,19 +4,19 @@ import GhostButton from '../components/ui/GhostButton.vue'
 </script>
 
 <template>
-  <div class="bg-background py-4 px-2 flex justify-between h-16">
+  <div class="bg-background py-4 px-2 flex justify-between">
     <div class="inline-flex gap-1">
       <img :src="whisprLogo" alt="" class="w-10 h-10" />
       <h1 class="text-xl text-primary font-extrabold">Whispr</h1>
     </div>
-    <div>
+    <!-- <div>
       <ul class="hidden md:inline-flex gap-4">
         <li>Features</li>
         <li>Pricings</li>
         <li>Docs</li>
         <li>Blog</li>
       </ul>
-    </div>
+    </div> -->
     <GhostButton title="Get Started" />
   </div>
 </template>
