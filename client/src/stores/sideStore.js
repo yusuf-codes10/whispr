f0cd45716ce1c16249c234185b43e494e462933d
Yusef Codes <youcefcopy@gmail.com>
2026-08-26 20:25:18 +0100

    SideStore updated: adding more actions

--- client/src/stores/sideStore.js
+++ client/src/stores/sideStore.js
@@ -2,21 +2,31 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
 export const useSideStore = defineStore('sidebar', () => {
+  // Desktop: is the rail expanded (w-60) or collapsed (w-17.5)?
   const isOpen = ref(false)
-  const isMobile = ref(false)
+
+  // Mobile: is the slide-in drawer showing?
+  // Previously called `isMobile`, which collided with the "viewport is mobile"
+  // computed in SideBar.vue and made the responsive behaviour hard to reason about.
+  const isDrawerOpen = ref(false)
 
   const toggle = () => {
     isOpen.value = !isOpen.value
   }
 
-  const toggleMobile = () => {
-    isMobile.value = !isMobile.value
+  const toggleDrawer = () => {
+    isDrawerOpen.value = !isDrawerOpen.value
+  }
+
+  const closeDrawer = () => {
+    isDrawerOpen.value = false
   }
 
   return {
     isOpen,
-    isMobile,
+    isDrawerOpen,
     toggle,
-    toggleMobile,
+    toggleDrawer,
+    closeDrawer,
   }
 })
